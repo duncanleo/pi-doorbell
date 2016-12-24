b85046e7042040376338d52375ee35a24a045fb5
Duncan Leo <duncanleo97@gmail.com>
2016-12-24 23:09:30 +0800

    Use mpg123 instead of omxplayer

--- sound.py
+++ sound.py
@@ -1,6 +1,6 @@
 import os, subprocess
 
 def play_sound(path):
-    play_process = subprocess.Popen(['omxplayer', '-o', 'local', os.path.realpath(path)], stdin=subprocess.PIPE, close_fds=True)
+    play_process = subprocess.Popen(['mpg123', '-q', os.path.realpath(path)], stdin=subprocess.PIPE, close_fds=True)
     # Wait for process to end
     play_process.wait()
