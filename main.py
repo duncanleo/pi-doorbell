dc914ef157ecb742bec5ac7646027d3d792d95ca
Duncan Leo <duncanleo97@gmail.com>
2018-07-13 07:45:16 +0800

    enumerate ./sounds instead of using txt file

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO;
 from time import sleep
-from os import path
-import storage, sound
+from os import path, listdir
+from random import choice
+import sound
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -9,7 +10,7 @@ GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def callback(channel):
 	print "Press detected! Doorbell..."
-	sound.play_sound(path.realpath(path.join("./sounds", storage.get_sound_file())))
+	sound.play_sound(path.realpath(path.join("./sounds", choice(listdir('./sounds')))))
 
 GPIO.add_event_detect(11, GPIO.RISING, callback=callback, bouncetime=300)
 
--- storage.py
+++
@@ -1,5 +0,0 @@
-import os
-
-def get_sound_file():
-    with open(os.path.realpath("./sound.txt")) as f:
-		return f.readlines()[0].replace("\r\n", "").replace("\n", "")
