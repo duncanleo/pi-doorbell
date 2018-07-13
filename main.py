9b8f086826eba85c4ccd05a23ea30a5c28bb03e1
Duncan Leo <duncanleo97@gmail.com>
2018-07-13 14:02:58 +0800

    Revert "use event callbacks"
    This reverts commit a30c3b2430aa581d68b9d633999b03834f0f3e68.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+<<<<<<< HEAD
 def callback(channel):
 	print "Press detected! Doorbell..."
 	music_files = listdir('./sounds')
@@ -16,5 +17,11 @@ def callback(channel):
 
 GPIO.add_event_detect(11, GPIO.RISING, callback=callback, bouncetime=1500)
 
+=======
+print "Starting the wait"
+>>>>>>> parent of a30c3b2... use event callbacks
 while True:
-	pass
+	if (GPIO.input(11) == False):
+		print "Press detected! Doorbell..."
+		sound.play_sound(path.realpath(path.join("./sounds", storage.get_sound_file())))
+	sleep(0.2)
