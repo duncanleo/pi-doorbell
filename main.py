d328039524187007d367a3a079fe3e94d8239cb4
Duncan Leo <duncanleo97@gmail.com>
2018-07-13 14:03:40 +0800

    reinstate random music selection

--- main.py
+++ main.py
@@ -8,20 +8,11 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-<<<<<<< HEAD
-def callback(channel):
-	print "Press detected! Doorbell..."
-	music_files = listdir('./sounds')
-	music_file = choice(filter(lambda x: x != ".gitignore", music_files))
-	sound.play_sound(path.realpath(path.join("./sounds", music_file)))
-
-GPIO.add_event_detect(11, GPIO.RISING, callback=callback, bouncetime=1500)
-
-=======
 print "Starting the wait"
->>>>>>> parent of a30c3b2... use event callbacks
 while True:
 	if (GPIO.input(11) == False):
 		print "Press detected! Doorbell..."
-		sound.play_sound(path.realpath(path.join("./sounds", storage.get_sound_file())))
+		music_files = listdir('./sounds') 
+		music_file = choice(filter(lambda x: x != ".gitignore", music_files)) 
+		sound.play_sound(path.realpath(path.join("./sounds", music_file))) 
 	sleep(0.2)
