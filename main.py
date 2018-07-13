cfd40861ed97403d4f261e57f9a0dc02554d17f0
Duncan Leo <duncanleo97@gmail.com>
2018-07-13 14:07:46 +0800

    add optional http request

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-import RPi.GPIO as GPIO;
+import RPi.GPIO as GPIO
 from time import sleep
-from os import path, listdir
+from os import path, listdir, environ
 from random import choice
 import sound
+import urllib2
+import urllib
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+post_data = urllib.urlencode({
+	'ding': 'dong',
+	'dong': 'ding'
+})
 
 print "Starting the wait"
 while True:
@@ -14,5 +21,9 @@ while True:
 		print "Press detected! Doorbell..."
 		music_files = listdir('./sounds') 
 		music_file = choice(filter(lambda x: x != ".gitignore", music_files)) 
-		sound.play_sound(path.realpath(path.join("./sounds", music_file))) 
+		sound.play_sound(path.realpath(path.join("./sounds", music_file)))
+
+		if environ['REQUEST_URL']:
+			req = urllib.Request(environ['REQUEST_URL'], post_data)
+			urllib2.urlopen(req).read()
 	sleep(0.2)
