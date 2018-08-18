2c8590845e9c9230eb5423c6a087a9a4c64c1c2d
Duncan Leo <duncanleo97@gmail.com>
2018-08-18 17:53:24 +0800

    Make HTTP request before song

--- main.py
+++ main.py
@@ -19,11 +19,10 @@ print "Starting the wait"
 while True:
 	if (GPIO.input(11) == False):
 		print "Press detected! Doorbell..."
-		music_files = listdir('./sounds') 
-		music_file = choice(filter(lambda x: x != ".gitignore", music_files)) 
-		sound.play_sound(path.realpath(path.join("./sounds", music_file)))
-
 		if environ['REQUEST_URL']:
 			req = urllib2.Request(environ['REQUEST_URL'], post_data)
 			urllib2.urlopen(req).read()
+		music_files = listdir('./sounds') 
+		music_file = choice(filter(lambda x: x != ".gitignore", music_files)) 
+		sound.play_sound(path.realpath(path.join("./sounds", music_file)))	
 	sleep(0.2)
